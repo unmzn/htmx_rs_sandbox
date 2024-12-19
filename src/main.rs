af466806e8bb37b73b1544431672e6f14ea1fa43
Alex Webb <alex.webb@prokeep.com>
2024-12-18 23:58:18 -0700

    delicious madness

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, routing::put, Router};
 use maud::{html, Markup, DOCTYPE};
 
 fn header() -> Markup {
@@ -27,6 +27,11 @@ fn button_box() -> Markup {
     html! {
         div style="background-color:#02e;width:500px;height:500px;" {
             button id="button" _="on click transition opacity to 0 then remove closest parent <div/>" { "Click me!" }
+            button hx-put="/messages" { "Load Messages" }
+        }
+        div style="background-color:#e20;width:500px;height:500px;" {
+            button _="on click toggle .bordered on #second-button" { "Toggle Next Border" }
+            button id="second-button" _="on click toggle .red unless I match .bordered" { "Toggle My Background" }
         }
     }
 }
@@ -41,10 +46,23 @@ async fn root_page() -> Markup {
     }
 }
 
+async fn messages() -> Markup {
+    html! {
+        div {
+            h1 { "Messages" }
+            ul {
+                li { "Hello, World!" }
+                li { "Goodbye, World!" }
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(root_page));
+    let app = Router::new().route("/", get(root_page))
+        .route("/messages", put(messages));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
